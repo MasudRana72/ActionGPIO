d83781124425259e067fde68452427fe9be60320
MASUD RANA <45023506+MasudRana72@users.noreply.github.com>
2023-10-13 00:43:33 +0600

    Update runnercode.py Popin

--- runnercode.py
+++ runnercode.py
@@ -3,6 +3,6 @@ import subprocess
 script_path = "/home/masud/actions-runner/_work/ActionGPIO/ActionGPIO/blink.py"
 
 try:
-    subprocess.run(["python3", script_path], check=True, cwd="/home/masud/actions-runner/_work/ActionGPIO/ActionGPIO")
+    process = subprocess.Popen(["python3", script_path], check=True, cwd="/home/masud/actions-runner/_work/ActionGPIO/ActionGPIO")
 except subprocess.CalledProcessError as e:
     print(f"Error running the script: {e}")
