01f212a26c7171d8111be8a4de9ef4570e8464c2
MASUD RANA <45023506+MasudRana72@users.noreply.github.com>
2023-10-12 13:18:23 +0600

    Update blink.py 1:18

--- blink.py
+++ blink.py
@@ -10,7 +10,7 @@ GPIO.setup(pinLED, GPIO.OUT)    # GPIO pin as output pin
 while True:                          # Endless Loop
     GPIO.output(pinLED, GPIO.HIGH)   # Turn on
     #print(LED on)                    # Prints state to console
-    sleep(2)                         # Pause 1 second
+    sleep(0.5)                         # Pause 1 second
     GPIO.output(pinLED, GPIO.LOW)    # Turn off
     #print(LED off)                   # Prints state to console
-    sleep(1)      
+    sleep(0.51)      
