6fbb367f5e6c1d4d762af05b3fdf2b622c3fbc86
MASUD RANA <45023506+MasudRana72@users.noreply.github.com>
2023-10-13 00:22:52 +0600

    Create runnercode.py

---
+++ runnercode.py
@@ -0,0 +1,8 @@
+import subprocess
+
+script_path = "/home/masud/actions-runner/_work/ActionGPIO/ActionGPIO/blink.py"
+
+try:
+    subprocess.run(["python3", script_path], check=True, cwd="/home/masud/actions-runner/_work/ActionGPIO/ActionGPIO")
+except subprocess.CalledProcessError as e:
+    print(f"Error running the script: {e}")
