318ee7bd1c27784077dc4b0eb00fc226df0e8590
MASUD RANA <45023506+MasudRana72@users.noreply.github.com>
2023-10-13 00:55:38 +0600

    Delete runnercode.py

--- runnercode.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-script_path = "/home/masud/actions-runner/_work/ActionGPIO/ActionGPIO/blink.py"
-
-try:
-    process = subprocess.Popen(["python3", script_path], check=True, cwd="/home/masud/actions-runner/_work/ActionGPIO/ActionGPIO")
-except subprocess.CalledProcessError as e:
-    print(f"Error running the script: {e}")
