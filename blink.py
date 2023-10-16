8551e12485329f10c34c371545bb460ef308bd87
MASUD RANA <45023506+MasudRana72@users.noreply.github.com>
2023-10-17 01:23:00 +0600

    Update blink.py

--- blink.py
+++ blink.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO         # Import Raspberry Pi GPIO library
 from time import sleep          # Import the sleep function 
-#Hellow Masud watchdog test
+#Hellow Masud watchdog test 
 pinLED = 4                      # LED GPIO Pin
 
 GPIO.setmode(GPIO.BCM)          # Use GPIO pin number
@@ -10,7 +10,7 @@ GPIO.setup(pinLED, GPIO.OUT)    # GPIO pin as output pin
 while True:                          # Endless Loop
     GPIO.output(pinLED, GPIO.HIGH)   # Turn on
     #print(LED on)                    # Prints state to console
-    sleep(0.5)                         # Pause 1 second
+    sleep(0.2)                         # Pause 1 second
     GPIO.output(pinLED, GPIO.LOW)    # Turn off
     #print(LED off)                   # Prints state to console
-    sleep(0.5)      
+    sleep(0.2)      
